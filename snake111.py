d01e8c844f10b0baa9415230a04b11f3c0eb82af
Tim Gallagher <tadgallagher@gmail.com>
2021-05-12 20:32:07 -0400

    Add files via upload

--- snake111.py
+++ snake111.py
@@ -1,50 +1,82 @@
 import pygame
+import time
  
 pygame.init()
  
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
+blue = (0, 0, 255)
  
-dis = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('Snake Game by Edureka')
+dis_width = 800
+dis_height = 600 
  
-game_over = False
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Snake Game by Tim')
  
-x1 = 300
-y1 = 300
+
+snake_block = 10
  
-x1_change = 0       
-y1_change = 0
- 
+
 clock = pygame.time.Clock()
+snake_speed = 30
+
+font_style = pygame.font.SysFont(None, 50)  
+
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width/3, dis_height/3])
+    
+    
+def gameLoop():
+    
+    game_over = False
+     
+    x1 = dis_width / 2
+    y1 = dis_height / 2         
  
-while not game_over:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    x1_change = 0       
+    y1_change = 0
+     
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_block
+                    x1_change = 0
+
+        # Check for boundries
+        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
             game_over = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x1_change = -10
-                y1_change = 0
-            elif event.key == pygame.K_RIGHT:
-                x1_change = 10
-                y1_change = 0
-            elif event.key == pygame.K_UP:
-                y1_change = -10
-                x1_change = 0
-            elif event.key == pygame.K_DOWN:
-                y1_change = 10
-                x1_change = 0
- 
-    x1 += x1_change
-    y1 += y1_change
-    dis.fill(white)
-    pygame.draw.rect(dis, black, [x1, y1, 10, 10])
- 
+        
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(white)
+        pygame.draw.rect(dis, black, [x1, y1, snake_block, snake_block])
+    
+        pygame.display.update()
+    
+        clock.tick(snake_speed)
+        
+    message("You lost", red)    
     pygame.display.update()
- 
-    clock.tick(30)
- 
-pygame.quit()
-quit()
+    time.sleep(2)
+    
+    pygame.quit()
+    quit()
+    
+    
+    
+# Main Part
+gameLoop()    
